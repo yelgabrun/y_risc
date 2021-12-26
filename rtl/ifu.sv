2a36fbfcf9e230c6683f5fdf01b6c20975dcec84
yelgabrun <yelgabrun1@gmail.com>
2021-12-26 07:43:54 +0000

    Added a reset_value_i input to the 32-bit register module

--- rtl/ifu.sv
+++ rtl/ifu.sv
@@ -14,11 +14,13 @@ module ifu (
   // Program Counter 32-bit register
   reg32bit reg32bit_pc_inst(.clk  (clk),
                             .rst_n(rst_n),
-                            .wr_en(1'b1), // PC write_enable tied high for now
+                            //.reset_value_i(32'h80000180),
+                            .reset_value_i(32'h00000000), // Location of first instruction
+                            .wr_en_i(1'b1), // PC write_enable tied high for now
                             .reg_i(next_pc),
                             .reg_o(pc));
 
-  assign next_pc = pc + 32'd4; // Add 4 bytes for the next Program Counter
+  assign next_pc = pc + 32'd4; // Add 4 bytes for the next instruction address
 
   instruction_memory instruction_memory_inst(.address_i(pc),
                                              .instruction_o(instruction_o));
--- rtl/reg32bit.sv
+++ rtl/reg32bit.sv
@@ -3,19 +3,20 @@
 module reg32bit (
   input        clk,
   input        rst_n,
-  input        wr_en,
+  input [31:0] reset_value_i,
+  input        wr_en_i,
   input  wire [31:0] reg_i,
   output reg  [31:0] reg_o
 );
 
-  
+
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
-      reg_o <= 32'h00;
-    else if (wr_en)
+      reg_o <= reset_value_i;
+    else if (wr_en_i)
       reg_o <= reg_i;
     else
       reg_o <= reg_o;
   end
-    
+
 endmodule
